96c3934a022dfbfee8cae3d6478ccbbc6ec1f3fa
Thiago Bezerra de Carvalho <tb161372@gmail.com>
2022-09-16 21:19:24 -0300

    fiz a mais

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Escreva algo que o nosso grande cebolinha diria!");
+
+string palavra = Console.ReadLine()!;
+
+string palavla = palavra
+
+.Replace("r","l")
+.Replace("R","L")
+.Replace("r","L");
+
+Console.WriteLine($"ele com certeza diria: {palavla}");
